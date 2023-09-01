8e911cbdac59d4683f97a4ea8bd08adcabce6f72
rhuaannn <rhuanmarques1223@gmail.com>
2023-08-31 23:00:00 -0300

    feat: create template gulp

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+const gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const autoprefixer = require("gulp-autoprefixer");
+const browserSync = require("browser-sync").create();
+
+function compilaSass() {
+  return gulp
+    .src("scss/*.scss")
+    .pipe(sass({ outputStyle: "compressed" }))
+    .pipe(
+      autoprefixer({
+        overrideBrowserslist: ["last 2 versions"],
+        cascade: false,
+      })
+    )
+    .pipe(gulp.dest("css/"))
+    .pipe(browserSync.stram());
+}
+
+gulp.task("sass", compilaSass);
+
+function browser() {
+  browserSync.init({
+    server: {
+      baseDir: "./",
+    },
+  });
+}
+gulp.task("browser-sync", browser);
+
+function watch() {
+  gulp.watch("scss/*.scss", compilaSass);
+  gulp.watch("*.html").on('change', browserSync.reload);
+
+}
+gulp.task('watch', watch)
+
+gulp.task("default", gulp.parallel('watch', 'browser-sync'));
